a008cf32b057c0ec9632a94e93448ff12edac87f
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 01:30:13 +0500

    dashboard container import/export in containers/index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,9 @@
 import _HomeContainer from "./home";
 import _LoginContainer from "./login";
 import _PageNotFound from "./404";
+import _Dashboard from "./dashboard";
 
 export const HomeContainer = _HomeContainer;
 export const LoginContainer = _LoginContainer;
 export const PageNotFound = _PageNotFound;
+export const Dashboard = _Dashboard;

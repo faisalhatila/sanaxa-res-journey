66f44ffa89571e45a6189a2b424b097be4ee6a45
Faisal Hatila <hatilas1994@gmail.com>
2020-06-15 22:19:03 +0500

    private route bug fixed

--- src/routers/privateRoute.js
+++ src/routers/privateRoute.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route as ReactRouter, Redirect } from "react-router-dom";
 import { Footer, Header, Layout } from "../components";
 
-const PrivateRoute = props => {
+const PrivateRoute = (props) => {
   const isAuthorized = true;
   if (!isAuthorized) return <Redirect to="/404" />;
   const withoutHeaderAndFooter = props.withoutHeaderAndFooter;
   const schema = (
     <React.Fragment>
-      {!withoutHeaderAndFooter && <Header />}
+      {withoutHeaderAndFooter && <Header />}
       <ReactRouter {...props} />
-      {!withoutHeaderAndFooter && <Footer />}
+      {withoutHeaderAndFooter && <Footer />}
     </React.Fragment>
   );
   if (props.withoutLayout) return schema;

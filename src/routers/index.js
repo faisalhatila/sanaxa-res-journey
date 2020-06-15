eef597383f52abae24129bedfa26991261c31e67
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 01:29:28 +0500

    dashboard route created

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import { Switch, Redirect } from "react-router-dom";
 import Route from "./route";
 import PvtRoute from "./privateRoute";
-import { HomeContainer, LoginContainer, PageNotFound } from "../containers";
+import {
+  HomeContainer,
+  LoginContainer,
+  PageNotFound,
+  Dashboard,
+} from "../containers";
 const Router = (props) => {
   return (
     <Switch>
       <Route exact path="/" component={HomeContainer} />
       <PvtRoute exact path="/login" component={LoginContainer} />
+      <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/404" component={PageNotFound} />
       <Route exact path="/*" component={() => <Redirect to="/404" />} />
     </Switch>

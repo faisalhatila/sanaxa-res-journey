8ab17f64258dd18bcd88c0d6b70181905fb87ab6
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 00:42:24 +0500

    top menu import, export in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,10 @@ import _Header from "./header";
 import _Footer from "./footer";
 import _Layout from "./layout";
 import _LeftMenu from "./leftmenu";
+import _TopMenu from "./topmenu";
 
 export const Header = _Header;
 export const Footer = _Footer;
 export const Layout = _Layout;
 export const LeftMenu = _LeftMenu;
+export const TopMenu = _TopMenu;

4594d46b8da3966f64138e798b3aa96ce9d36802
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 00:40:19 +0500

    left menu import, export in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import _Header from "./header";
 import _Footer from "./footer";
 import _Layout from "./layout";
+import _LeftMenu from "./leftmenu";
 
 export const Header = _Header;
 export const Footer = _Footer;
 export const Layout = _Layout;
+export const LeftMenu = _LeftMenu;

733bb9dc53dfcef3b6b991ac22fc04b2ce6830f3
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 23:44:52 +0500

    data counter component, initialized

--- src/components/datacounter/index.js
+++ src/components/datacounter/index.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const DataCounter = () => {
   return (
-    <div className="dataCounterComponentMainDiv">
-      <div className="container">
-        <div className="col-12 col-lg-8 offser-lg-4 col-md-8 offset-md-4">
-          <p>hi</p>
-        </div>
-      </div>
-    </div>
+    // <div className="dataCounterComponentMainDiv">
+    //   <div className="container">
+    //     <div className="col-12 col-lg-10 offser-lg-2 col-md-10 offset-md-10 mt-4">
+    //       <p>hasdmnasdklnasklfasjkldnasklfnsdkfbaskjjgakdfjgkjsfni</p>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="col-12 col-lg-9 col-md-9">Hello</div>
   );
 };
 

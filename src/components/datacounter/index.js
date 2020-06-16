ce26c8c2d9b8c5119b65cf68c428c78b0996b6f4
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 23:04:08 +0500

    data counter component created

---
+++ src/components/datacounter/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DataCounter = () => {
+  return (
+    <div className="dataCounterComponentMainDiv">
+      <div className="container">
+        <div className="col-12 col-lg-8 offser-lg-4 col-md-8 offset-md-4">
+          <p>hi</p>
+        </div>
+      </div>
+    </div>
+  );
+};

fa9e019c6f0d66ba46398a08f7df16b4e1d79103
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 23:42:47 +0500

    leftmenu component fixed

--- src/components/leftmenu/index.js
+++ src/components/leftmenu/index.js
@@ -2,28 +2,38 @@ import React from "react";
 
 const LeftMenu = () => {
   return (
-    <div className="mt-4">
+    <div className="mt-4 col-3">
       <div className="container leftMenuContainer">
-        <div className="col-2 leftMenuMainDiv">
+        <div className="leftMenuMainDiv">
           <h2>
             <strong>Snaxa</strong>
           </h2>
           <div className="leftMenuMenuLinksMainDiv">
             <ul>
               <li className="mt-3">
-                <i class="fas fa-border-all mr-4"></i>Dashboard
+                <div>
+                  <i class="fas fa-border-all mr-4"></i>Dashboard
+                </div>
               </li>
               <li className="mt-3">
-                <i class="fas fa-border-all mr-4"></i>Dashboard
+                <div>
+                  <i class="fas fa-border-all mr-4"></i>Dashboard
+                </div>
               </li>
               <li className="mt-3">
-                <i class="fas fa-border-all mr-4"></i>Dashboard
+                <div>
+                  <i class="fas fa-border-all mr-4"></i>Dashboard
+                </div>
               </li>
               <li className="mt-3">
-                <i class="fas fa-border-all mr-4"></i>Dashboard
+                <div>
+                  <i class="fas fa-border-all mr-4"></i>Dashboard
+                </div>
               </li>
-              <li className="mt-3">
-                <i class="fas fa-border-all mr-4"></i>Dashboard
+              <li className="mt-3 d-flex align-items-center justify-content-between">
+                <div>
+                  <i class="fas fa-border-all mr-4"></i>Dashboard
+                </div>
                 <i class="fas fa-caret-down ml-4"></i>
               </li>
             </ul>
@@ -31,6 +41,7 @@ const LeftMenu = () => {
         </div>
       </div>
     </div>
+    // <div className="col-lg-2 col-md-2">Hello</div>
   );
 };
 

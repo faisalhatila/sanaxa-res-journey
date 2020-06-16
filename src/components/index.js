fbb8a1f912cf612320fd15aedb69a980f4184586
Faisal Hatila <hatilas1994@gmail.com>
2020-06-17 00:09:16 +0500

    line chart component import, export in component/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import _Layout from "./layout";
 import _LeftMenu from "./leftmenu";
 import _TopMenu from "./topmenu";
 import _DataCounter from "./datacounter";
+import _LineChart from "./linechartcomponent";
 
 export const Header = _Header;
 export const Footer = _Footer;
@@ -11,3 +12,4 @@ export const Layout = _Layout;
 export const LeftMenu = _LeftMenu;
 export const TopMenu = _TopMenu;
 export const DataCounter = _DataCounter;
+export const LineChart = _LineChart;

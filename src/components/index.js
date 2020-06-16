4f56630bb8bbd0cf984ff60885df60566e88bd24
Faisal Hatila <hatilas1994@gmail.com>
2020-06-16 23:05:21 +0500

    data counter component import, export in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@ import _Footer from "./footer";
 import _Layout from "./layout";
 import _LeftMenu from "./leftmenu";
 import _TopMenu from "./topmenu";
+import _DataCounter from "./datacounter";
 
 export const Header = _Header;
 export const Footer = _Footer;
 export const Layout = _Layout;
 export const LeftMenu = _LeftMenu;
 export const TopMenu = _TopMenu;
+export const DataCounter = _DataCounter;

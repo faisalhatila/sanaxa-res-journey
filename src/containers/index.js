14215fe50fb0284badb8a524f2aa09c26337e62c
Faisal Hatila <hatilas1994@gmail.com>
2020-06-17 18:27:16 +0500

    customer management container import, export in containers/index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,8 +2,10 @@ import _HomeContainer from "./home";
 import _LoginContainer from "./login";
 import _PageNotFound from "./404";
 import _Dashboard from "./dashboard";
+import _CustomerManagement from "./customermanagement";
 
 export const HomeContainer = _HomeContainer;
 export const LoginContainer = _LoginContainer;
 export const PageNotFound = _PageNotFound;
 export const Dashboard = _Dashboard;
+export const CustomerManagement = _CustomerManagement;

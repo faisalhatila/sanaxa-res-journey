e0b2191dc9c286762807bfecc4e3e09106a811d9
Faisal Hatila <hatilas1994@gmail.com>
2020-06-17 19:34:21 +0500

    add customer container import, export in containers/index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,9 +3,11 @@ import _LoginContainer from "./login";
 import _PageNotFound from "./404";
 import _Dashboard from "./dashboard";
 import _CustomerManagement from "./customermanagement";
+import _AddCustomer from "./addcustomer";
 
 export const HomeContainer = _HomeContainer;
 export const LoginContainer = _LoginContainer;
 export const PageNotFound = _PageNotFound;
 export const Dashboard = _Dashboard;
 export const CustomerManagement = _CustomerManagement;
+export const AddCustomer = _AddCustomer;

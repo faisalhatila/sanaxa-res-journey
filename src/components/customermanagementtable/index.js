92ea3aa3b065e7e9c9d6b932227f7d55ff90058e
Faisal Hatila <hatilas1994@gmail.com>
2020-06-17 18:24:51 +0500

    customer management table component initialized

---
+++ src/components/customermanagementtable/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CustomerManagementTable extends Component {
+  render() {
+    return <div className="conatainer"> Table</div>;
+  }
+}

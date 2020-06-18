7254545533716e0897fa55edc1b173016fb293ce
Faisal Hatila <hatilas1994@gmail.com>
2020-06-18 21:55:13 +0500

    restaurant management table rendered to restaurant management container

--- src/containers/restaurantmanagement/index.js
+++ src/containers/restaurantmanagement/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { LeftMenu, CustomerManagementTable } from "../../components";
+import { LeftMenu, RestaurantManagementTable } from "../../components";
 
 export default class RestaurantManagement extends Component {
   render() {
@@ -8,7 +8,7 @@ export default class RestaurantManagement extends Component {
         <div className="row">
           <LeftMenu />
           <div className="col-12 col-lg-9 col-md-9 mt-4 customermanagementtable">
-            <CustomerManagementTable />
+            <RestaurantManagementTable />
           </div>
         </div>
       </div>

c6adcbc5190144887465ee5afe2ed73969c3b957
Faisal Hatila <hatilas1994@gmail.com>
2020-06-18 21:51:37 +0500

    restaurant management container import , export in containers/index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,6 +5,7 @@ import _Dashboard from "./dashboard";
 import _CustomerManagement from "./customermanagement";
 import _AddCustomer from "./addcustomer";
 import _Register from "./register";
+import _RestaurantManagement from "./restaurantmanagement";
 
 export const HomeContainer = _HomeContainer;
 export const LoginContainer = _LoginContainer;
@@ -13,3 +14,4 @@ export const Dashboard = _Dashboard;
 export const CustomerManagement = _CustomerManagement;
 export const AddCustomer = _AddCustomer;
 export const Register = _Register;
+export const RestaurantManagement = _RestaurantManagement;

2ba3417124baea46ff36a1ffadfc20985606ff57
Faisal Hatila <hatilas1994@gmail.com>
2020-06-19 00:52:34 +0500

    order management container done

--- src/containers/ordermanagement/index.js
+++ src/containers/ordermanagement/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { LeftMenu, RestaurantManagementTable } from "../../components";
+import { LeftMenu, OrderManagementTable } from "../../components";
 
 export default class RestaurantManagement extends Component {
   render() {
@@ -8,7 +8,7 @@ export default class RestaurantManagement extends Component {
         <div className="row">
           <LeftMenu />
           <div className="col-12 col-lg-9 col-md-9 mt-4 customermanagementtable">
-            <RestaurantManagementTable />
+            <OrderManagementTable />
           </div>
         </div>
       </div>

ea8bf310b24c36dd775c6d6b8dd847f0e9bdedf3
Faisal Hatila <hatilas1994@gmail.com>
2020-06-20 02:22:55 +0500

    left menu css fixed

--- src/components/leftmenu/index.js
+++ src/components/leftmenu/index.js
@@ -12,56 +12,72 @@ const LeftMenu = () => {
             <ul>
               <li className="mt-3">
                 <div>
-                  {/* <i class="fas fa-border-all mr-4"></i> */}
+                  {/* <i class="fas fa-border-all mr-2"></i> */}
                   <img
                     src="assets/img/icons/leftMenu/dashboard.svg"
-                    className="mr-4"
+                    className="mr-2"
                   />
                   Dashboard
                 </div>
               </li>
-              <li className="mt-3">
+              {/* <li className="mt-3">
                 <div>
-                  {/* <i class="fas fa-border-all mr-4"></i> */}
+                  <i class="fas fa-border-all mr-2"></i>
                   <img
                     src="assets/img/icons/leftMenu/customerManagement.svg"
-                    className="mr-4"
+                    className="mr-2"
                   />
                   Customer Management
                 </div>
+              </li> */}
+              <li className="mt-3 d-flex align-items-center justify-content-between">
+                <div>
+                  {/* <i class="fas fa-border-all mr-2"></i> */}
+                  <img
+                    src="assets/img/icons/leftMenu/customerManagement.svg"
+                    className="mr-2"
+                  />
+                  Customer Management
+                </div>
+                <img
+                  src="assets/img/icons/leftMenu/dropdown.svg"
+                  className="mr-2"
+                />
+
+                {/* <i class="fas fa-caret-down ml-4"></i> */}
               </li>
               <li className="mt-3">
                 <div>
-                  {/* <i class="fas fa-border-all mr-4"></i> */}
+                  {/* <i class="fas fa-border-all mr-2"></i> */}
                   <img
                     src="assets/img/icons/leftMenu/restaurantManagement.svg"
-                    className="mr-4"
+                    className="mr-2"
                   />
                   Restaurant Management
                 </div>
               </li>
               <li className="mt-3">
                 <div>
-                  {/* <i class="fas fa-border-all mr-4"></i> */}
+                  {/* <i class="fas fa-border-all mr-2"></i> */}
                   <img
                     src="assets/img/icons/leftMenu/orderManagement.svg"
-                    className="mr-4"
+                    className="mr-2"
                   />
                   Order Management
                 </div>
               </li>
               <li className="mt-3 d-flex align-items-center justify-content-between">
                 <div>
-                  {/* <i class="fas fa-border-all mr-4"></i> */}
+                  {/* <i class="fas fa-border-all mr-2"></i> */}
                   <img
                     src="assets/img/icons/leftMenu/kitchenManagement.svg"
-                    className="mr-4"
+                    className="mr-2"
                   />
-                  Dashboard
+                  Kitchen Management
                 </div>
                 <img
                   src="assets/img/icons/leftMenu/dropdown.svg"
-                  className="mr-4"
+                  className="mr-2"
                 />
 
                 {/* <i class="fas fa-caret-down ml-4"></i> */}

d5bd5a20bb4f5e2b4c90e3e958f74f02e5638446
Faisal Hatila <hatilas1994@gmail.com>
2020-06-20 02:36:40 +0500

    counter component fixed

--- src/components/datacounter/index.js
+++ src/components/datacounter/index.js
@@ -2,41 +2,40 @@ import React from "react";
 
 const DataCounter = () => {
   return (
-    // <div className="dataCounterComponentMainDiv">
-    //   <div className="container">
-    //     <div className="col-12 col-lg-10 offser-lg-2 col-md-10 offset-md-10 mt-4">
-    //       <p>hasdmnasdklnasklfasjkldnasklfnsdkfbaskjjgakdfjgkjsfni</p>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="">
       <div className="row dataCounterRow">
         <div className="col-6 col-lg-3 col-md-3">
           <div className="counterRowDataCols">
-            <i class="fas fa-hand-holding-usd counterRowDataIcons"></i>
-            <p className="counterRowDataValues">$10,275</p>
-            <p className="counterRowDataType">Total Sales</p>
+            <img src="assets/img/icons/dataCounter/totalSales.svg" />
+            {/* <i class="fas fa-hand-holding-usd counterRowDataIcons"></i> */}
+            <p className="counterRowDataValues totalSales">$10,275</p>
+            <p className="counterRowDataType totalSales">Total Sales</p>
           </div>
         </div>
         <div className="col-6 col-lg-3 col-md-3">
           <div className="counterRowDataCols">
-            <i class="fas fa-hand-holding-usd counterRowDataIcons"></i>
-            <p className="counterRowDataValues">$10,275</p>
-            <p className="counterRowDataType">Total Sales</p>
+            {/* <i class="fas fa-hand-holding-usd counterRowDataIcons"></i> */}
+            <img src="assets/img/icons/dataCounter/totalCustomers.svg" />
+            <p className="counterRowDataValues totalCustomers">$10,275</p>
+            <p className="counterRowDataType totalCustomers">Total Customers</p>
           </div>
         </div>
         <div className="col-6 col-lg-3 col-md-3">
           <div className="counterRowDataCols">
-            <i class="fas fa-hand-holding-usd counterRowDataIcons"></i>
-            <p className="counterRowDataValues">$10,275</p>
-            <p className="counterRowDataType">Total Sales</p>
+            {/* <i class="fas fa-hand-holding-usd counterRowDataIcons"></i> */}
+            <img src="assets/img/icons/dataCounter/totalOrders.svg" />
+            <p className="counterRowDataValues totalOrders">$10,275</p>
+            <p className="counterRowDataType totalOrders">Total Orders</p>
           </div>
         </div>
         <div className="col-6 col-lg-3 col-md-3">
           <div className="counterRowDataCols">
-            <i class="fas fa-hand-holding-usd counterRowDataIcons"></i>
-            <p className="counterRowDataValues">$10,275</p>
-            <p className="counterRowDataType">Total Sales</p>
+            {/* <i class="fas fa-hand-holding-usd counterRowDataIcons"></i> */}
+            <img src="assets/img/icons/dataCounter/totalRestaurants.svg" />
+            <p className="counterRowDataValues totalRestaurants">$10,275</p>
+            <p className="counterRowDataType totalRestaurants">
+              Total Restaurants
+            </p>
           </div>
         </div>
       </div>

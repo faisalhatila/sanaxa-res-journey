726c1d6ca55e5168186283e086c8bb8416105626
Faisal Hatila <hatilas1994@gmail.com>
2020-06-29 18:43:03 +0500

    add addons container created

--- src/containers/addaddons/index.js
+++ src/containers/addaddons/index.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 
 export default class AddAddons extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="row">
+          <LeftMenu />
+          <div className="col-12 col-lg-9 col-md-9 mt-4">
+            {/* <NewCustomer /> */}
+          </div>
+        </div>
+      </div>
+    );
   }
 }

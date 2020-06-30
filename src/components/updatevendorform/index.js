70b2cc9b0d5187f3aadbe5f30779458e7ec2d1af
Faisal Hatila <hatilas1994@gmail.com>
2020-06-30 18:24:16 +0500

    update vendor form validation fixed

--- src/components/updatevendorform/index.js
+++ src/components/updatevendorform/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { first } from "lodash";
 
 export default class UpdateVendorForm extends Component {
   state = {
@@ -60,7 +61,135 @@ export default class UpdateVendorForm extends Component {
       websiteOrSocialLink: event.target.value,
     });
   };
+  validate = () => {
+    const {
+      firstName,
+      lastName,
+      mobileNo,
+      email,
+      storeName,
+      storeType,
+      storeLocation,
+    } = this.state;
+    let {
+      firstNameError,
+      lastNameError,
+      mobileNoError,
+      emailError,
+      storeNameError,
+      storeTypeError,
+      storeLocationError,
+    } = this.state;
+    if (!firstName) {
+      firstNameError = "Please Enter First Name";
+    } else {
+      firstNameError = "";
+    }
+    if (!lastName) {
+      lastNameError = "Please Enter Last Name";
+    } else {
+      lastNameError = "";
+    }
+    if (!mobileNo) {
+      mobileNoError = "Please Enter Mobile No";
+    } else {
+      mobileNoError = "";
+    }
+    if (!email || !email.includes("@")) {
+      emailError = "Please Enter Valid Email";
+    } else {
+      emailError = "";
+    }
+    if (!storeName) {
+      storeNameError = "Please Enter Store Name";
+    } else {
+      storeNameError = "";
+    }
+    if (!storeType) {
+      storeTypeError = "Please Enter Store Type";
+    } else {
+      storeTypeError = "";
+    }
+    if (!storeLocation) {
+      storeLocationError = "Please Enter Store Location";
+    } else {
+      storeLocationError = "";
+    }
+    if (
+      firstNameError ||
+      lastNameError ||
+      mobileNoError ||
+      emailError ||
+      storeNameError ||
+      storeTypeError ||
+      storeLocationError
+    ) {
+      this.setState({
+        firstNameError,
+        lastNameError,
+        mobileNoError,
+        emailError,
+        storeNameError,
+        storeTypeError,
+        storeLocationError,
+      });
+      return false;
+    }
+    return true;
+  };
+  handleSubmit = (event) => {
+    const {
+      firstName,
+      lastName,
+      mobileNo,
+      email,
+      storeName,
+      storeType,
+      storeLocation,
+      websiteOrSocialLink,
+    } = this.state;
+    event.preventDefault();
+    console.log(this.state);
+
+    const isValid = this.validate();
+    if (isValid) {
+      let collections = [
+        firstName,
+        lastName,
+        mobileNo,
+        email,
+        storeName,
+        storeType,
+        storeLocation,
+        websiteOrSocialLink,
+      ];
+      let { data } = this.state;
+      data.push(collections);
+      console.log(data);
+
+      setTimeout(() => {
+        alert("perfect");
+      }, 2000);
+    }
+  };
   render() {
+    const {
+      firstName,
+      lastName,
+      mobileNo,
+      email,
+      storeName,
+      storeType,
+      storeLocation,
+      websiteOrSocialLink,
+      firstNameError,
+      lastNameError,
+      mobileNoError,
+      emailError,
+      storeNameError,
+      storeTypeError,
+      storeLocationError,
+    } = this.state;
     return (
       <div className="row">
         <div className="col-4 col-lg-3 col-md-3 updateVendorFormTitle">
@@ -79,8 +208,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleFirstNameChange}
-                    //   placeholder="Enter email"
+                    placeholder="First Name"
+                    value={firstName}
                   />
+                  {firstNameError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {firstNameError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Last Name</label>
@@ -90,8 +231,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleLastNameChange}
-                    //   placeholder="Enter email"
+                    placeholder="Last Name"
+                    value={lastName}
                   />
+                  {lastNameError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {lastNameError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Mobile No</label>
@@ -101,8 +254,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleMobNoChange}
-                    //   placeholder="Enter email"
+                    placeholder="Mobile No"
+                    value={mobileNo}
                   />
+                  {mobileNoError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {mobileNoError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Email Address</label>
@@ -112,8 +277,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleEmailChange}
-                    //   placeholder="Enter email"
+                    placeholder="Enter Email"
+                    value={email}
                   />
+                  {emailError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {emailError}
+                    </div>
+                  ) : null}
                 </div>
               </div>
               <div className="col-12 col-lg-6 col-md-6 updateVendorForm">
@@ -126,8 +303,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleStoreNameChange}
-                    //   placeholder="Enter email"
+                    placeholder="Enter Store Name"
+                    value={storeName}
                   />
+                  {storeNameError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {storeNameError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Store Type</label>
@@ -137,8 +326,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleStoreTypeChange}
-                    //   placeholder="Enter email"
+                    placeholder="Enter Store Type"
+                    value={storeType}
                   />
+                  {storeTypeError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {storeTypeError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Store Location</label>
@@ -148,8 +349,20 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleStoreLocationChange}
-                    //   placeholder="Enter email"
+                    placeholder="Enter Store Location"
+                    value={storeLocation}
                   />
+                  {storeLocationError ? (
+                    <div
+                      style={{
+                        textAlign: "center",
+                        color: "red",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {storeLocationError}
+                    </div>
+                  ) : null}
                 </div>
                 <div class="form-group">
                   <label for="exampleInputEmail1">Website/Social Media</label>
@@ -159,14 +372,21 @@ export default class UpdateVendorForm extends Component {
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                     onChange={this.handleWebsiteOrSocialLinkChange}
-                    //   placeholder="Enter email"
+                    placeholder="Enter Website / Social Link"
+                    value={firstName}
                   />
                 </div>
               </div>
             </div>
-            <button type="submit" class="btn btn-primary mt-3">
-              Submit
-            </button>
+            <div>
+              <button
+                type="submit"
+                class="btn btn-primary mt-3"
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
           </form>
         </div>
       </div>

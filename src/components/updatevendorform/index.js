62b4be0d195daaf3c8cd347cf9641dab169a784c
Faisal Hatila <hatilas1994@gmail.com>
2020-06-30 17:34:42 +0500

    handle change functions linked and for ui grid fixed

--- src/components/updatevendorform/index.js
+++ src/components/updatevendorform/index.js
@@ -66,95 +66,107 @@ export default class UpdateVendorForm extends Component {
         <div className="col-4 col-lg-3 col-md-3 updateVendorFormTitle">
           Store Details
         </div>
-        <div className="col-12 customerDetailFormMainDiv d-lg-flex d-md-flex">
-          <form className="col-12 col-lg-6 col-md-6 updateVendorForm">
-            <h5>Vendor Detail</h5>
-            <div class="form-group">
-              <label for="exampleInputEmail1">First Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Last Name</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Mobile No</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email Address</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
+        <div className="col-12 customerDetailFormMainDiv">
+          <form className="">
+            <div className="d-flex">
+              <div className="col-12 col-lg-6 col-md-6 updateVendorForm">
+                <h5>Vendor Detail</h5>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">First Name</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleFirstNameChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Last Name</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleLastNameChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Mobile No</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleMobNoChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email Address</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleEmailChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+              </div>
+              <div className="col-12 col-lg-6 col-md-6 updateVendorForm">
+                <h5>Store Detail</h5>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Store Name</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleStoreNameChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Store Type</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleStoreTypeChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Store Location</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleStoreLocationChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Website/Social Media</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={this.handleWebsiteOrSocialLinkChange}
+                    //   placeholder="Enter email"
+                  />
+                </div>
+              </div>
             </div>
             <button type="submit" class="btn btn-primary mt-3">
               Submit
             </button>
-          </form>
-          <form className="col-12 col-lg-6 col-md-6 updateVendorForm">
-            <h5>Store Detail</h5>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Store Name</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Store Type</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Store Location</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Website/Social Media</label>
-              <input
-                type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                //   placeholder="Enter email"
-              />
-            </div>
           </form>
         </div>
       </div>

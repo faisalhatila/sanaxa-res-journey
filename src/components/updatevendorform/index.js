55c6dc82418f1bba69b50bd5e69844943b075143
Faisal Hatila <hatilas1994@gmail.com>
2020-06-30 17:12:52 +0500

    update form state created

--- src/components/updatevendorform/index.js
+++ src/components/updatevendorform/index.js
@@ -18,6 +18,7 @@ export default class UpdateVendorForm extends Component {
     storeTypeError: "",
     storeLocationError: "",
     websiteOrSocialLink: "",
+    data: [],
   };
   render() {
     return (
